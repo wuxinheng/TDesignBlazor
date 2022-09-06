d3d3b80377d5e875dc877cc61f0b60e2c259462e
吴新恒 <17629957093@163.com>
2022-09-06 13:34:32 +0800

    update

---
+++ TDesignBlazor.5x/Components/Button/Button.razor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace TDesignBlazor._5x.Components.Button
+{
+    public partial class Button: ComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public string Text { get; set; }
+        [Parameter]
+        public string Color { get; set; }
+        [Parameter]
+        public string Width { get; set; }
+        [Parameter]
+        public string Height { get; set; }
+
+        protected override void OnInitialized()
+        {
+            
+            base.OnInitialized();
+            Color = "color:red;";
+            Width = "width:10px;";
+            Height = "height:10px;";
+        }
+    }
+}
---
+++ TDesignBlazor/Core/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDesignBlazor.Core.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddTDesign(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
