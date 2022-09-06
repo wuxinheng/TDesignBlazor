48fb6689d9ecc732a0d0facfbb5006f8647c2e1d
吴新恒 <17629957093@163.com>
2022-09-06 17:36:32 +0800

    update

---
+++ BlazorServerApp/Pages/Button.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlazorServerApp.Pages
+{
+    public partial class Button : ComponentBase
+    {
+
+    }
+}
---
+++ src/TDesignBlazor.6x/Component/Button/TButton.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TDesignBlazor._6x.Component
+{
+    public partial class TButton : ComponentBase
+    {
+        private string _class;
+        [Parameter]
+        public string Class { get => _class; set => _class = value; }
+
+        private string _style;
+        [Parameter]
+        public string Style { get => _style; set => _style = value; }
+
+        private string _text;
+        [Parameter]
+        public string Text { get => _text; set => _text = value; }
+
+        protected override void OnInitialized()
+        {
+            Class = "";
+            Style = "color:red;";
+            Text = "1234";
+            base.OnInitialized();
+        }
+    }
+}
