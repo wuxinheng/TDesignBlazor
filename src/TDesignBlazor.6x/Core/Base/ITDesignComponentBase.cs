11719d6f8462fa4d53617c092e8a6d0f0313db7d
wuxinheng <17629957093@163.com>
2022-09-07 22:33:14 +0800

    update

---
+++ src/TDesignBlazor.6x/Core/Base/ITDesignComponentBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDesignBlazor._6x.Core.Base
+{
+    public interface ITDesignComponentBase
+    {
+    }
+}
---
+++ src/TDesignBlazor.6x/Core/Base/TDesignComponentBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDesignBlazor._6x.Core.Base
+{
+    public class TDesignComponentBase : ComponentBase, ITDesignComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
