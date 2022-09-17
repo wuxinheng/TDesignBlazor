baff802b37b1275d45f2ea9b696df786dcc4ff39
wuxinheng <17629957093@163.com>
2022-09-17 23:04:07 +0800

    update

--- src/TDesignBlazor.6x/Component/Grid/GutterType.cs
+++ src/TDesignBlazor.6x/Component/Grid/GutterType.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using OneOf;
+﻿using OneOf;
 
-namespace TDesignBlazor._6x.Component.Grid
+
+
+
+namespace TDesignBlazor._6x.Component
 {
-
 
     /// <summary>
     /// 响应式
---
+++ src/TDesignBlazor.6x/Component/Grid/TRow.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using OneOf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using OneOf;
+using TDesignBlazor._6x.Core.Base;
+using System;
+
+
+namespace TDesignBlazor._6x.Component
+{
+    using ResponsiveBlend = OneOf.OneOf<int, System.Collections.Generic.IEnumerable<TDesignBlazor._6x.Component.ResponsiveSetting>>;
+    public partial class TRow : TDesignComponentBase
+    {
+
+        [Parameter]
+        public string Align { get; set; }
+        [Parameter]
+        public string Justify { get; set; }
+        [Parameter]
+        public string Tag { get; set; }
+        [Parameter]
+        public OneOf<int, int[], ResponsiveSetting, IEnumerable<ResponsiveSetting>, IEnumerable<ResponsiveBlend>> Gutter { get; set; }
+
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+        }
+    }
+}
